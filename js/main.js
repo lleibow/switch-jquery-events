32c3d5d9b1dfe9eaed4585fa6a0239037d96686a
LAURA LEIBOW <lauraleibow@gmail.com>
2017-02-15 16:23:18 -0500

    when clicked, if class is 'on', the class toggles from on to of

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+$(function() {
+  $('.switch button').on('click', function(){
+    if ($('button').attr('class') === 'on') {
+      $('button').toggleClass('on off');
+    };
+  });
+});
 
-
+// else ($('button').attr('class')==='off'){
+//   $('button').toggleClass('off on');
+// };
