70790272ce3131f1a55dab574f5ce28d4952ac9a
LAURA LEIBOW <lauraleibow@gmail.com>
2017-02-15 16:39:24 -0500

    added if else statement so it toggles from off to on as well

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ $(function() {
   $('.switch button').on('click', function(){
     if ($('button').attr('class') === 'on') {
       $('button').toggleClass('on off');
-    };
+    }
+    else {
+      $('button').toggleClass('off on');
+    }
   });
 });
-
-// else ($('button').attr('class')==='off'){
-//   $('button').toggleClass('off on');
-// };
