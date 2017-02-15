c4b96d7f5871bc5add77d0d2f774e1f25d13b3f0
LAURA LEIBOW <lauraleibow@gmail.com>
2017-02-15 17:02:09 -0500

    text changes! finalllyyy

--- js/main.js
+++ js/main.js
@@ -2,5 +2,12 @@ $(function() {
   $('.switch button').on('click', function(){
     $('body').toggleClass('dark light');
     $('button').toggleClass('on off');
+
+    if ($('button').attr('class') === 'on') {
+      $('h1').text("It's so bright in here");
+    }
+    else {
+      $('h1').text("Yoinks, it's dark!");
+    }
 })
 })
